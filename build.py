9831b1de8678fb3a5c745a138a9885708e080f37
dhruvpeter <dhruveldhopeter@gmail.com>
2021-04-25 21:04:05 +0530

    Change branch coverage and branch partial coverage threshold

--- build.py
+++ build.py
@@ -17,3 +17,5 @@ default_task = "publish"
 def set_properties(project):
     project.build_depends_on("mockito")
     project.set_property("coverage_threshold_warn", 90)
+    project.set_property("coverage_branch_threshold_warn", 80)
+    project.set_property("coverage_branch_partial_threshold_warn", 80)
