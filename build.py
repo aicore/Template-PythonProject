92cd29602c6f97633c762c6267d972ab73e43131
Ushmajit <ushmajit@gmail.com>
2021-06-02 15:15:16 +0530

    Flake8 integration for lint checks

--- build.py
+++ build.py
@@ -19,3 +19,9 @@ def set_properties(project):
     project.set_property("coverage_threshold_warn", 90)
     project.set_property("coverage_branch_threshold_warn", 80)
     project.set_property("coverage_branch_partial_threshold_warn", 80)
+    #Properties to fail build on Flake8 warnings
+    project.build_depends_on('flake8')
+    project.set_property('flake8_verbose_output', True)
+    project.set_property("flake8_break_build", True)
+    project.set_property("flake8_include_test_sources", True)
+    project.set_property("flake8_include_scripts", True)
